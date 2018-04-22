b896467c109c6fd2b455f0c3d46cac0382565005
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-21 21:46:39 -0500

    Add config fields for global data
    - viewport boundaries
    - screen dimensions
    - viewport offset

--- client/js/config.js
+++ client/js/config.js
@@ -1,5 +1,27 @@
-export default {
+import player from './player';
+
+const config = {
+  // the canvas only takes up the screen
+  get screenWidth() { return window.innerWidth; },
+  get screenHeight() { return window.innerHeight; },
+
+  // but the game area is larger
   gameWidth: 5000,
   gameHeight: 5000,
-  gridScale: 80,
+
+  // size of the grid cells
+  gridScale: window.innerHeight / 12,
+
+  // viewport offset
+  offset: { x: 0, y: 0 },
+
+  // viewport boundaries
+  viewport: {
+    get left() { return player.currentPlayer.pos.x - (config.screenWidth / 2); },
+    get top() { return player.currentPlayer.pos.y - (config.screenHeight / 2); },
+    get right() { return player.currentPlayer.pos.x + (config.screenWidth / 2); },
+    get bottom() { return player.currentPlayer.pos.y + (config.screenHeight / 2); },
+  },
 };
+
+export default config;
