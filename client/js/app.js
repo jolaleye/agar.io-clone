ac7ab9c937cc84264b32e7b6542a5706bb38f945
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-21 21:48:30 -0500

    "handleSocket", "update", and "draw" delegate logic

--- client/js/app.js
+++ client/js/app.js
@@ -1,28 +1,33 @@
+import socket from './index';
 import canvas from './canvas';
+import player from './player';
 
-class App {
-  animate = () => {
-    window.requestAnimationFrame(this.animate);
-    canvas.reset();
+export const handleSocket = () => {
+
+};
+
+export const update = () => {
+  canvas.update();
+};
+
+export const draw = () => {
+
+};
+
+export const changeOverlaysTo = mode => {
+  const startOverlay = document.querySelector('.start.overlay');
+  const gameOverlay = document.querySelector('.game.overlay');
+  const endOverlay = document.querySelector('.end.overlay');
+
+  startOverlay.classList.add('hidden');
+  gameOverlay.classList.add('hidden');
+  endOverlay.classList.add('hidden');
+
+  if (mode === 'start') {
+    startOverlay.classList.remove('hidden');
+  } else if (mode === 'game') {
+    gameOverlay.classList.remove('hidden');
+  } else if (mode === 'end') {
+    endOverlay.classList.remove('hidden');
   }
-
-  changeOverlaysTo = mode => {
-    const startOverlay = document.querySelector('.start.overlay');
-    const gameOverlay = document.querySelector('.game.overlay');
-    const endOverlay = document.querySelector('.end.overlay');
-
-    startOverlay.classList.add('hidden');
-    gameOverlay.classList.add('hidden');
-    endOverlay.classList.add('hidden');
-
-    if (mode === 'start') {
-      startOverlay.classList.remove('hidden');
-    } else if (mode === 'game') {
-      gameOverlay.classList.remove('hidden');
-    } else if (mode === 'end') {
-      endOverlay.classList.remove('hidden');
-    }
-  }
-}
-
-export default new App();
+};
