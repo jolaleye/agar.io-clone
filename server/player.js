be801e9e246b7dfa7488af9aa55e3facbc93c2e8
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-21 21:41:53 -0500

    Assign players random positions

--- server/player.js
+++ server/player.js
@@ -1,7 +1,14 @@
+const _ = require('lodash');
+
 class Player {
   constructor(id, name) {
     this.id = id;
     this.name = name;
+
+    this.pos = {
+      x: _.random(5000),
+      y: _.random(5000),
+    };
   }
 }
 
