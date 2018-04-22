a7c2996d893d9f8e47c07fda0fca1f3f6c745e7c
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-22 18:56:47 -0500

    Adjust starting mass and drag

--- server/player.js
+++ server/player.js
@@ -6,7 +6,7 @@ class Player {
   constructor(id, name) {
     this.id = id;
     this.name = name;
-    this.mass = 10;
+    this.mass = 5;
 
     this.pos = {
       x: _.random(config.gameWidth),
@@ -19,7 +19,7 @@ class Player {
     const yDistance = target.y - this.pos.y;
 
     // greater mass slows the player
-    const drag = this.mass * 10;
+    const drag = Math.min(this.mass * 15, 500);
     // player moves faster when the target is farther away
     const xVel = xDistance / drag;
     const yVel = yDistance / drag;
