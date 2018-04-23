de7571b257e97c3c6aea8deef294da590a2c4936
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-22 21:46:16 -0500

    Check for food

--- server/player.js
+++ server/player.js
@@ -7,6 +7,7 @@ class Player {
     this.id = id;
     this.name = name;
     this.mass = 5;
+    this.radius = this.mass * 10;
 
     this.pos = {
       x: _.random(config.gameWidth),
@@ -19,13 +20,27 @@ class Player {
     const yDistance = target.y - this.pos.y;
 
     // greater mass slows the player
-    const drag = Math.min(this.mass * 15, 500);
+    const drag = Math.min(this.mass * 25, 1000);
     // player moves faster when the target is farther away
     const xVel = xDistance / drag;
     const yVel = yDistance / drag;
 
     this.pos.x += xVel;
     this.pos.y += yVel;
+  }
+
+  checkFood(foods) {
+    foods.forEach((food, i) => {
+      const xDistance = this.pos.x - food.pos.x;
+      const yDistance = this.pos.y - food.pos.y;
+      const distance = Math.sqrt((xDistance ** 2) + (yDistance ** 2));
+
+      if (distance < this.radius) {
+        this.mass += 1;
+        this.radius = this.mass * 10;
+        foods.splice(i, 1);
+      }
+    });
   }
 }
 
