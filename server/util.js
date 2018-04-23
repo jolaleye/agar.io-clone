4fc2003acf7d5f9648c86b9e3118dda73ad7df65
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-23 15:27:01 -0500

    Add distance util function

--- server/util.js
+++ server/util.js
@@ -31,4 +31,10 @@ exports.getLeaders = (players, currentPlayer) => {
   return sortedPlayers;
 };
 
+exports.getDistance = (x1, x2, y1, y2) => ({
+  x: x2 - x1,
+  y: y2 - y1,
+  total: Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2)),
+});
+
 module.exports = exports;
