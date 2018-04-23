4eb67919b47ec2b833e7da878aaaa25a1fdf439a
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-22 21:45:30 -0500

    Add communication for food, score, and leaderboard

--- client/js/app.js
+++ client/js/app.js
@@ -2,19 +2,27 @@ import socket from './index';
 import canvas from './canvas';
 import player from './player';
 import ui from './ui';
+import food from './food';
 
 export const handleSocket = () => {
   socket.on('players', players => { player.players = players; });
   socket.on('moveTo', pos => { player.currentPlayer.pos = pos; });
+  socket.on('food', newFood => { food.food = newFood; });
+  socket.on('score', score => ui.updateScore(score));
+  socket.on('leaders', leaders => ui.updateLeaderboard(leaders));
 };
 
 export const update = () => {
   canvas.update();
   socket.emit('requestPlayers');
   socket.emit('requestMove', ui.target);
+  socket.emit('requestFood');
+  socket.emit('requestScore');
+  socket.emit('requestLeaders');
 };
 
 export const draw = () => {
   canvas.reset();
   player.drawPlayers();
+  food.drawFood();
 };
