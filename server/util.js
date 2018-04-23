25078b7d75c4d4a320f5a9b9a4adf33277fd8eb9
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-23 14:16:56 -0500

    Move leader function to util

--- server/util.js
+++ server/util.js
@@ -15,4 +15,20 @@ exports.createFood = count => {
   return food;
 };
 
+exports.getLeaders = (players, currentPlayer) => {
+  const sortedPlayers = _.sortBy(Object.values(players), p => p.mass).reverse();
+
+  // give every player their rank
+  // eslint-disable-next-line
+  sortedPlayers.forEach((sortedPlayer, i) => { sortedPlayer.rank = i + 1; });
+
+  // cut down to 10 players
+  if (sortedPlayers.length >= 10) sortedPlayers.splice(10);
+
+  // if the current player isn't in the top ten add them to the end
+  if (!_.includes(sortedPlayers, currentPlayer)) sortedPlayers.push(currentPlayer);
+
+  return sortedPlayers;
+};
+
 module.exports = exports;
