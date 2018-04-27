88c84234031bf1d50fe8b86c61c600fb7d368271
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-26 19:06:05 -0500

    Change wording from mass to radius

--- client/js/player.js
+++ client/js/player.js
@@ -7,7 +7,7 @@ class Player {
 
   updatePlayers = players => {
     this.players = Object.values(players);
-    this.currentPlayer = players[this.currentPlayer.id];
+    if (config.playing) this.currentPlayer = players[this.currentPlayer.id];
   }
 
   moveTo = pos => {
@@ -21,15 +21,15 @@ class Player {
     this.players.forEach(player => {
       const x = player.pos.x - config.offset.x;
       const y = player.pos.y - config.offset.y;
-      const { mass } = player;
-      const fontSize = Math.max(mass / 2, 30);
+      const radius = player.mass;
+      const fontSize = Math.max(radius / 2, 30);
 
       // cell
       c.fillStyle = player.fillColor;
       c.strokeStyle = player.strokeColor;
       c.lineWidth = 5;
       c.beginPath();
-      c.arc(x, y, mass, 0, 2 * Math.PI);
+      c.arc(x, y, radius, 0, 2 * Math.PI);
       c.fill();
       c.stroke();
       c.closePath();
