00fdf19341303554f4273bd3cb66b96b05e7ab8c
Jonathan Olaleye <j.olaleye.o@gmail.com>
2018-04-26 19:04:50 -0500

    Modify actions on death, add mass-related functions

--- client/js/app.js
+++ client/js/app.js
@@ -1,29 +1,39 @@
 import socket from './index';
+import config from './config';
 import canvas from './canvas';
 import player from './player';
 import ui from './ui';
 import food from './food';
+import mass from './mass';
 
 export const handleSocket = () => {
   socket.on('players', players => player.updatePlayers(players));
   socket.on('moveTo', pos => player.moveTo(pos));
   socket.on('food', newFood => food.updateFood(newFood));
+  socket.on('masses', masses => mass.updateMasses(masses));
   socket.on('score', score => ui.updateScore(score));
   socket.on('leaders', leaders => ui.updateLeaderboard(leaders));
-  socket.on('death', () => ui.endGame());
+  socket.on('death', () => {
+    config.playing = false;
+    ui.endGame();
+  });
 };
 
 export const update = () => {
-  canvas.update();
+  if (config.playing) {
+    canvas.update();
+    socket.emit('requestMove', ui.target);
+  }
   socket.emit('requestPlayers');
-  socket.emit('requestMove', ui.target);
   socket.emit('requestFood');
   socket.emit('requestScore');
   socket.emit('requestLeaders');
+  socket.emit('requestMasses');
 };
 
 export const draw = () => {
   canvas.reset();
   food.drawFood();
   player.drawPlayers();
+  mass.drawMasses();
 };
